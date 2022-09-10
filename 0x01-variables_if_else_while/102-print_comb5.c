180f91d8f562cb17e80fbaef30537da910cfa092
CandyEsinam <candyesinam@gmail.com>
2022-09-09 19:32:59 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,23 +2,14 @@
 
 /**
 
- * main - print possible combination of two 2-digit
-
- * numbers
-
- *
-
- * Return: returns zero at the end
-
- */
-
+* main - print possible combination of two 2-digit
+* numbers
+*
+* Return: returns zero at the end
+*/
 int main(void)
-
 {
-
 	int i, p;
-
-
 
 	for (i = 0; i < 100; i++)
 	{
@@ -27,15 +18,10 @@ int main(void)
 			if (p > i)
 			{
 				putchar((i / 10) + '0');
-
 				putchar((i % 10) + '0');
-
 				putchar(' ');
-
 				putchar((p / 10) + '0');
-
 				putchar((p % 10) + '0');
-
 				if (i != 98)
 				{
 					putchar(',');
@@ -46,6 +32,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
