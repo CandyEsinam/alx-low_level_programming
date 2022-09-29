d3e68bc19e51b276f51702f22db58f25a3c13f82
CandyEsinam <candyesinam@gmail.com>
2022-09-29 12:23:09 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,10 +3,10 @@
 /**
  * _strlen_recursion - calculates the length of a string
  * @s: string to be used
- * 
+ *
  * Return: length of the string
  */
-void _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	int sum = 0;
 
@@ -16,5 +16,5 @@ void _strlen_recursion(char *s)
 		sum += _strlen_recursion(s + 1);
 	}
 	return (sum);
-}
 
+}
